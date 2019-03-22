ed1e65648fdeca894a54d5830b4bbce3de6caee6
unknown <yaroslav.khudchenko@gmail.com>
2019-03-22 07:25:46 +0100

    finish intro-> start jasmine

--- test.js
+++ test.js
@@ -4,4 +4,10 @@ function add(x, y) {
 
 add(2, 3); // 5
 add(2, 3); // 6
-add('2', '3');// '23'
+add('2', '3'); // '23'
+add('22',true); // error
+
+// tests below
+expect(add(2, 3).toBe(5));
+expect(add(2, 'test')).toThrow();
+expect(add(0.1, 0.2)).toBe(0.3);
